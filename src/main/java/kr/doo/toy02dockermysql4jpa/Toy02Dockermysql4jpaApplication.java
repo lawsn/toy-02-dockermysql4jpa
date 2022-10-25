a72cf721198a6c201e1003cc6057781d47fa0644
vincent.oh <lawsnland@gmail.com>
2022-10-25 20:12:16 +0900

    Initial commit

---
+++ src/main/java/kr/doo/toy02dockermysql4jpa/Toy02Dockermysql4jpaApplication.java
@@ -0,0 +1,13 @@
+package kr.doo.toy02dockermysql4jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy02Dockermysql4jpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Toy02Dockermysql4jpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/doo/toy02dockermysql4jpa/Toy02Dockermysql4jpaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.doo.toy02dockermysql4jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toy02Dockermysql4jpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
